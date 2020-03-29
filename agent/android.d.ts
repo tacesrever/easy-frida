a07b9e6f4483e18993e9f0d8b13fc3f97dca5f93
tacesrever <tacesrever@gmail.com>
2020-03-30 06:43:34 +0800

    add ts hint for libraryOnLoad

--- agent/android.d.ts
+++ agent/android.d.ts
@@ -10,9 +10,12 @@ export declare function backtrace(tidOrContext?: number | CpuContext): void;
  */
 export declare function showLogcat(level: number): void;
 /**
- * callback will be called when library loaded. \
- * callback(0) when init funcs not called, \
- * callback(1) after.
+ * callback will be called when library loaded.  
+ * callback(0) when init funcs not called,  
+ * callback(1) after.  
+ * for new user:  
+ * you should find `__dl__ZN6soinfo17call_constructorsEv` in `/system/bin/linker` | `/system/bin/linker64`  
+ * then setup them in android.js->libraryOnLoad.
  */
 export declare function libraryOnLoad(libname: string, callback: (inited: number) => void): void;
 
